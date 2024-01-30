e831e3130fc0bc6980dc519621ccd9b40a9b59ee
Mihailo Milenkovic <mmilenkovic@groq.com>
2024-01-30 01:58:24 -0800

    dummy mpi examples

---
+++ tmp/print_master.py
@@ -0,0 +1,15 @@
+import sys
+from mpi4py import MPI
+
+def main():
+    # Initialize MPI
+    comm = MPI.COMM_WORLD.Spawn(
+        sys.executable,  # Path to Python interpreter
+        args=['worker.py'],  # Arguments to the spawned script
+        maxprocs=4  # Number of processes to spawn
+    )
+    
+    print("Done")
+
+if __name__ == "__main__":
+    main()
